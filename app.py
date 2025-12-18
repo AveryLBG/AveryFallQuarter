aec0b497dbda38fef0e8209369dfa9d456eea6fc
Avery Garner <avery.garner@hgs.hiddengeniusproject.org>
2025-12-17 18:13:39 -0800

    END OF FALL WEBSITE

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ app = Flask(__name__)
 #a route to the home page
 @app.route('/')
 def index():
-    return "Welcome to my Flask App home page"
+    return render_template("index.html")
+
+@app.route('/scroll')
+def scroll():
+    return render_template("scroller.html")
 
 #a new route to the greet page of our website
 @app.route("/greet/<name>")
-def GreetPage(name):
-    return render_template("index.html", name=name)
+def GreetPage():
+    return "Welcome to my Flask App home page"
+
 
 if __name__ == '__main__':
     app.run(debug = True)
